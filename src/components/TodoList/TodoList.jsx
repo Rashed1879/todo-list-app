1877fcc8f8c296ad5fc94c83c798cb718cdd4a9b
Rashed1879 <rashedul15-1879@diu.edu.bd>
2024-02-12 13:21:53 +0600

    added filtering option based on priority

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,16 +1,52 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Todo from '../Todo/Todo';
 import { TodoContext } from '../../provider/TodoContext';
 
 const TodoList = () => {
 	const [todos] = useContext(TodoContext);
+	const [filteredTodos, setFilteredTodos] = useState(todos);
+
+	useEffect(() => {
+		setFilteredTodos(todos);
+	}, [todos]);
+
+	const handleFilter = (filterAs) => {
+		if (filterAs === 'all') {
+			setFilteredTodos(todos);
+		} else {
+			const filteredData = todos.filter(
+				(todo) => todo.priority === filterAs
+			);
+			setFilteredTodos(filteredData);
+		}
+	};
+
 	return (
 		<>
 			<div className="space-y-3">
-				{todos.length >= 1 ? (
-					todos.map((todo) => <Todo key={todo.id} todo={todo} />)
+				<div>
+					<select
+						name="filter"
+						defaultValue=""
+						onChange={(e) => handleFilter(e.target.value)}
+					>
+						<option value="" disabled>
+							Filter by Priority
+						</option>
+						<option value="all">All</option>
+						<option value="low">Low</option>
+						<option value="medium">Medium</option>
+						<option value="high">High</option>
+					</select>
+				</div>
+				{filteredTodos.length >= 1 ? (
+					filteredTodos.map((todo) => (
+						<Todo key={todo.id} todo={todo} />
+					))
 				) : (
-					<p>There is no todo. Please add some..</p>
+					<p className="text-xl font-bold">
+						There is no todo. Please add some..
+					</p>
 				)}
 			</div>
 		</>
